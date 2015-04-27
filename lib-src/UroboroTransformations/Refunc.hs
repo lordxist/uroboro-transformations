b97acb05dcd7d74ef8e52d2edf1cb10b715a9925
Julian Jabs <julian.jabs@web.de>
2015-04-27 11:41:14 +0200

    Finish implementing Refunc.elimDesFromMixeds

--- lib-src/UroboroTransformations/Refunc.hs
+++ lib-src/UroboroTransformations/Refunc.hs
@@ -12,12 +12,20 @@ import Control.Arrow(first)
 import Control.Monad.Trans.Writer.Lazy
 
 isMixedRules :: PT -> Bool
-isMixedRules pt = False -- TODO: implement
+isMixedRules (PTFun _ _ _ _ rs) = (any hasDesPattern rs) && (any hasConPattern rs)
+  where
+    hasDesPattern (PTRule _ (PQDes _ _ _ _) _) = True
+    hasDesPattern _                            = False
+
+    hasConPattern (PTRule _ (PQApp _ _ _) _) = True
+    hasConPattern _                          = False
+isMixedRules _                  = True
 
 extractAllDesCalls :: [PT] -> PT -> PTRule -> Writer HelperFuns PTRule
-extractAllDesCalls pts fun r@(PTRule l (PQDes _ _ _ _) _) = do
+extractAllDesCalls pts fun r@(PTRule _ (PQDes _ _ _ _) _) = do
     replacedRule <- extractDesCalls pts fun r
     extractAllDesCalls pts fun replacedRule
+extractAllDesCalls _ _ r = return r
 
 elimDesFromMixeds :: [PT] -> [PT]
 elimDesFromMixeds pts = uncurry (++) $ first (extractHelperFuns extractAllDesCalls) $ partition isMixedRules pts
--- main-src/UnmixR.hs
+++ main-src/UnmixR.hs
@@ -6,14 +6,12 @@ import Uroboro.Tree
 import Uroboro.Error
 import Uroboro.Checker
 
--- TODO: implement this module
---import UroboroTransformations.FragmentsForRefunc.MixedDefs.Refunc
+import UroboroTransformations.Refunc
 
 import System.IO
 import System.Environment
 
--- |Not implemented yet, later it:
--- parses, typechecks, unmixes the input for refunc., then typechecks and pretty-prints result
+-- |Parses, typechecks, unmixes the input for refunc., then typechecks and pretty-prints result
 -- For this to work, uroboro.cabal needs to be edited to expose Uroboro.PrettyPrint
 main :: IO ()
 main = do
@@ -25,7 +23,7 @@ main = do
         Right pts -> case typecheck pts of
             Left e -> hPutStrLn stderr $ unlines ["Typecheck Error: ", show e]
             Right _p -> do
-                let t = pts -- TODO: implement module above and use it here
+                let t = elimDesFromMixeds pts
                 case typecheck t of
                     Left e -> hPutStrLn stderr $ unlines ["Typecheck Error in transformed program: ", show e]
                     Right _ -> putStrLn $ renderProgram t
