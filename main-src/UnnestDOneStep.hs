c9fd759ec7be9e4476de28eb20dcf90701276077
Julian Jabs <julian.jabs@web.de>
2015-09-02 17:00:58 +0200

    Fix isDNestedFunDef

--- main-src/UnnestDOneStep.hs
+++ main-src/UnnestDOneStep.hs
@@ -37,6 +37,7 @@ isDNestedFunDef :: PT -> Bool
 isDNestedFunDef (PTFun _ _ _ _ rs) = any isDNestedRule rs
   where
     isDNestedRule (PTRule _ (PQDes _ _ _ (PQDes _ _ _ _)) _) = True
+    isDNestedRule (PTRule _ (PQDes _ _ pps' (PQApp _ _ pps)) _) = any con (pps++pps')
     isDNestedRule _ = False
 isDNestedFunDef _ = False
 
