74146fe85b059a06d4535d32bcdb2e532e66b679
Julian Jabs <julian.jabs@web.de>
2015-09-10 15:34:09 +0200

    Fix Unnest.ForRefunc.isRNestedFunDef

--- lib-src/UroboroTransformations/Unnest/ForRefunc.hs
+++ lib-src/UroboroTransformations/Unnest/ForRefunc.hs
@@ -13,7 +13,10 @@ isRNestedFunDef (PTFun _ _ _ _ rs) = any isRNestedRule rs
     isRNestedRule (PTRule _ pq _) = isRNestedLhs pq
 
     isRNestedLhs (PQApp _ _ pps) = (length $ findIndices con pps) > 1
-    isRNestedLhs (PQDes _ _ pps pq) = (any con pps) || (isRNestedLhs pq)
+    isRNestedLhs pq = any con (ppsInPQ pq)
+
+    ppsInPQ (PQApp _ _ pps) = pps
+    ppsInPQ (PQDes _ _ pps pq) = pps ++ (ppsInPQ pq)
 isRNestedFunDef _ = False
 
 unnestForRefunc :: [PT] -> Maybe [PT]
