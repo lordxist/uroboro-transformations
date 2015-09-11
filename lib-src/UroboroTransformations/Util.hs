8bc28528b4905b4cec0cb0b6915ea137918bdb5b
Julian Jabs <julian.jabs@web.de>
2015-09-11 17:46:18 +0200

    Remove legacy utility code

--- lib-src/UroboroTransformations/Util.hs
+++ lib-src/UroboroTransformations/Util.hs
@@ -107,22 +107,6 @@ funIdentifier :: PT -> Identifier
 funIdentifier (PTFun _ id _ _ _) = id
 funIdentifier _ = undefined
 
--- the old version of what is now autogen (see below)
-gensym :: Identifier -> Identifier -> [PT] -> Identifier
-gensym id1 id2 pts = (findUnusedIdPrefix 0) ++ "_" ++ id1 ++ "_" ++ id2
-  where
-    findUnusedIdPrefix n
-        | not (any (isPrefixOf pref) usedNames) = pref
-        | otherwise = findUnusedIdPrefix (n+1)
-        where pref = "autogen" ++ (show n)
-
-    usedNames = (map desIdentifier (concatMap destructors pts)) ++
-                    (map conIdentifier (concatMap constructors pts)) ++
-                    (map funIdentifier (filter isFunctionDef pts))
-
-    isFunctionDef (PTFun _ _ _ _ _) = True
-    isFunctionDef _ = False
-
 autogen :: Identifier -> [PT] -> Identifier
 autogen id pts = (findUnusedIdPrefix 0) ++ "_" ++ id
   where
@@ -137,17 +121,6 @@ autogen id pts = (findUnusedIdPrefix 0) ++ "_" ++ id
 
     isFunctionDef (PTFun _ _ _ _ _) = True
     isFunctionDef _ = False
-
--- Problem: not a unique mapping. Can be made to be one, but ideally the names should then
--- be simplified to a nicer form whenever there are no conflicts.
-namePattern :: PQ -> String
-namePattern (PQApp _ id pps) = id ++ "_" ++ (intercalate "__" (map namePPPattern pps))
-namePattern (PQDes _ id pps pq) =
-    id ++ "_" ++ (intercalate "__" (map namePPPattern pps)) ++ "__" ++ (namePattern pq)
-
-namePPPattern :: PP -> String
-namePPPattern (PPVar _ _) = ""
-namePPPattern (PPCon _ id pps) = id ++ "_" ++ (intercalate "__" (map namePPPattern pps))
 
 collectVars :: PP -> [PP]
 collectVars (PPCon _ _ pps) = concatMap collectVars pps
@@ -165,41 +138,10 @@ collectVarsTQ :: TQ -> [TP]
 collectVarsTQ (TQDes _ _ tps tq') = (collectVarsTQ tq') ++ (concatMap collectVarsT tps)
 collectVarsTQ (TQApp _ _ tps)     = concatMap collectVarsT tps
 
--- Requires globally unique constructor names. Otherwise collectVarTypes needs to be implemented
--- using typed syntax trees (and the transformation is run on the typecheck result).
-constructorTypes :: Identifier -> [PT] -> [Type]
-constructorTypes id pts = types ((concatMap getConstructorForId pts) !! 0)
-  where
-    getConstructorForId (PTPos l t ((con@(PTCon _ _ id' _)):cons))
-        | id' == id = [con]
-        | otherwise = getConstructorForId (PTPos l t cons)
-    getConstructorForId _ = []
-
-    types (PTCon _ _ _ ts) = ts
-
 getDestructorForId :: Identifier -> PT -> [PTDes]
 getDestructorForId id (PTNeg l t ((des@(PTDes _ _ id' _ _)):dess))
         | id' == id = [des]
         | otherwise = getDestructorForId id (PTNeg l t dess)
 getDestructorForId id _ = []
 
--- Analogous to constructorTypes.
-destructorTypes :: Identifier -> [PT] -> [Type]
-destructorTypes id pts = types ((concatMap (getDestructorForId id) pts) !! 0)
-  where
-    types (PTDes _ _ _ ts _) = ts
 
--- Analogous limitation to constructorTypes and destructorTypes.
-destructorReturnType :: Identifier -> [PT] -> Type
-destructorReturnType id pts = returnType ((concatMap (getDestructorForId id) pts) !! 0)
-  where
-    returnType (PTDes _ t _ _ _) = t
-
-varTypes :: [PT] -> (PP, Type) -> [Type]
-varTypes pts ((PPVar _ _), t)       = [t]
-varTypes pts ((PPCon _ id pps'), _) = concatMap (varTypes pts) (zip pps' (constructorTypes id pts))
-
-collectVarTypes :: [PT] -> [Type] -> PQ -> [Type]
-collectVarTypes pts ts (PQApp _ _ pps) = concatMap (varTypes pts) (zip pps ts)
-collectVarTypes pts ts (PQDes _ id pps pq) =
-    (collectVarTypes pts ts pq) ++ (concatMap (varTypes pts) (zip pps (destructorTypes id pts)))    
