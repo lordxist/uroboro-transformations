db00eac9247e35904d5fe4d7f8a62344a1a58ed9
Julian Jabs <julian.jabs@web.de>
2015-09-16 11:42:41 +0200

    Remove outcommented old code

--- lib-src/UroboroTransformations/Defunc.hs
+++ lib-src/UroboroTransformations/Defunc.hs
@@ -4,33 +4,6 @@ import Data.Maybe
 
 import Uroboro.Tree
 
--- old version
-{-
-import qualified UroboroTransformations.FragmentsForDefunc.MixedDefs.Defunc as MixedDefsD
-import qualified UroboroTransformations.FragmentsForDefunc.Entangled.Defunc as EntangledD
-
-import UroboroTransformations.Util
-import UroboroTransformations.Util.HelperFuns
-import UroboroTransformations.Util.DesExtraction
-
-import Control.Monad(liftM)
-import Control.Monad.Trans.Reader
-import Control.Monad.Trans.Writer.Lazy
-
-extractOuterDesCalls :: PT -> PTRule -> ReaderT [PT] (Writer HelperFuns) PTRule
-extractOuterDesCalls pt r@(PTRule l (PQDes _ _ _ pq@(PQDes l' _ _ _)) _e) = do
-    replacedRule <- extractDesCalls pt r
-    extractOuterDesCalls pt replacedRule
-extractOuterDesCalls _ r = return r
-
-elimMultiDes :: [PT] -> [PT]
-elimMultiDes = extractHelperFuns extractOuterDesCalls
-
--- |Defunctionalize an Uroboro program
-defunc :: [PT] -> Maybe [PT]
-defunc pts = EntangledD.defuncLegal $ MixedDefsD.desExtract $ elimMultiDes pts
--}
-
 import UroboroTransformations.Unnest.ForDefunc (unnestForDefunc)
 import qualified UroboroTransformations.CoreDR.CoreDefunc as CoreDefunc (defunc)
 
--- lib-src/UroboroTransformations/Refunc.hs
+++ lib-src/UroboroTransformations/Refunc.hs
@@ -2,42 +2,6 @@ module UroboroTransformations.Refunc where
 
 import Uroboro.Tree
 
--- old version
-{-
-import UroboroTransformations.Util.HelperFuns
-import UroboroTransformations.Util.DesExtraction
-
-import qualified UroboroTransformations.FragmentsForRefunc.Entangled.Refunc as EntangledR
-
-import Data.List(partition)
-import Control.Arrow(first)
-import Control.Monad.Trans.Reader
-import Control.Monad.Trans.Writer.Lazy
-
-isMixedRules :: PT -> Bool
-isMixedRules (PTFun _ _ _ _ rs) = (any hasDesPattern rs) && (any hasConPattern rs)
-  where
-    hasDesPattern (PTRule _ (PQDes _ _ _ _) _) = True
-    hasDesPattern _                            = False
-
-    hasConPattern (PTRule _ (PQApp _ _ _) _) = True
-    hasConPattern _                          = False
-isMixedRules _                  = True
-
-extractAllDesCalls :: PT -> PTRule -> ReaderT [PT] (Writer HelperFuns) PTRule
-extractAllDesCalls fun r@(PTRule _ (PQDes _ _ _ _) _) = do
-    replacedRule <- extractDesCalls fun r
-    extractAllDesCalls fun replacedRule
-extractAllDesCalls _ r = return r
-
-elimDesFromMixeds :: [PT] -> [PT]
-elimDesFromMixeds pts = uncurry (++) $ first (extractHelperFuns extractAllDesCalls) $ partition isMixedRules pts
-
--- |Refunctionalize an Uroboro program
-refunc :: [PT] -> Maybe [PT]
-refunc pts = EntangledR.refuncLegal $ elimDesFromMixeds pts
--}
-
 import UroboroTransformations.Unnest.ForRefunc (unnestForRefunc)
 import UroboroTransformations.CoreDR.MoveCon (moveConFront)
 import qualified UroboroTransformations.CoreDR.CoreRefunc as CoreRefunc (refunc)
