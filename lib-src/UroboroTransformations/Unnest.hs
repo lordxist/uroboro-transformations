7a26ff503aeee60edc3b49eb3bc065799aa2e30a
Julian Jabs <julian.jabs@web.de>
2015-09-21 10:29:37 +0200

    Remove debug trace

--- lib-src/UroboroTransformations/Unnest.hs
+++ lib-src/UroboroTransformations/Unnest.hs
@@ -23,8 +23,6 @@ import Control.Monad.State.Lazy
 import Control.Monad.Writer.Lazy
 import Control.Monad.Trans.Maybe
 
-import Debug.Trace
-
 -- |Describes whether the function definition is not sufficiently unnested for some purpose.
 type UnnestPredicate = PT -> Bool
 
@@ -59,7 +57,7 @@ ptToSig (PTFun l id ts t rs) = (id, (l, ts, t))
 unnesting :: Program -> [PT] -> PT -> State [PT] (Maybe PT)
 unnesting prog pts pt = Data.Traversable.sequence $ do
     c <- runReader (checkCoverage (ptToSig pt) pt) prog
-    return $ evalStateT (flip runReaderT pts $ unnestingWithCoverage pt) (trace (show c) c)
+    return $ evalStateT (flip runReaderT pts $ unnestingWithCoverage pt) c
 
 -- |Unnest any Uroboro program with copattern coverage.
 -- Fails when the program doesn't have copattern coverage.
